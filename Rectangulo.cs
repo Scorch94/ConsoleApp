b9c8b4ff2ff66042ed0e739521ba952656eec5f0
José Pedro Costa <jpcgcosta@gmail.com>
2024-04-10 15:03:17 +0000

    José

---
+++ Rectangulo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Retangulo
+{
+    private double _base;
+    private double _altura;
+
+    public Retangulo(double bas = 1, double alt = 1)
+    {
+        Base = bas;
+        Altura = alt;
+    }
+
+    public double Base
+    {
+        get { return _base; }
+        set
+        {
+            if (value > 0)
+                _base = value;
+            else
+                throw new ArgumentOutOfRangeException("O valor da base deve ser maior que zero.");
+        }
+    }
+
+    public double Altura
+    {
+        get { return _altura; }
+        set
+        {
+            if (value > 0)
+                _altura = value;
+            else
+                throw new ArgumentOutOfRangeException("O valor da altura deve ser maior que zero.");
+        }
+    }
+
+    public double CalcularArea()
+    {
+        return _base * _altura;
+    }
+
+    public double CalcularPerimetro()
+    {
+        return 2 * (_base + _altura);
+    }
+}
+
