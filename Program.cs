672157c9c2c4bf05ac8026d1398413030f58e2ba
Eric Alves Sena <ericsena241@gmail.com>
2024-04-10 16:04:17 +0200

    Eric
    Eric

--- Program.cs
+++ Program.cs
@@ -4,10 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Quadrado quadrado = new Quadrado(5); // Criando um quadrado com lado de comprimento 5
+            Quadrado quadrado = new Quadrado(4); // Criando um quadrado com lado de comprimento 5
             Console.WriteLine($"Perímetro do quadrado: {quadrado.Perimetro()}");
             Console.WriteLine($"Área do quadrado: {quadrado.Area()}");
             Console.WriteLine($"Diagonal do quadrado: {quadrado.Diagonal()}");
+
+            try
+            {
+                double baseRetangulo = 5;
+                double alturaRetangulo = 3;
+
+                Retangulo retangulo = new Retangulo(baseRetangulo, alturaRetangulo);
+
+                Console.WriteLine("Área do retângulo: " + retangulo.CalcularArea());
+                Console.WriteLine("Perímetro do retângulo: " + retangulo.CalcularPerimetro());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocorreu uma exceção: " + ex.Message);
+            }
         }
     }
 }
