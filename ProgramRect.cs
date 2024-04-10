1c3c12ecc13f20b0278789c06d11f053f285bcd6
José Pedro Costa <jpcgcosta@gmail.com>
2024-04-10 15:07:10 +0000

    José

---
+++ ProgramRect.cs
@@ -0,0 +1,24 @@
+﻿namespace Rectangulo
+{
+
+    class ProgramRect
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double baseRetangulo = 5;
+                double alturaRetangulo = 3;
+
+                Retangulo retangulo = new Retangulo(baseRetangulo, alturaRetangulo);
+
+                Console.WriteLine("Área do retângulo: " + retangulo.CalcularArea());
+                Console.WriteLine("Perímetro do retângulo: " + retangulo.CalcularPerimetro());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocorreu uma exceção: " + ex.Message);
+            }
+        }
+    }
+}
